cc94097f474c5b1a7c00669526768ae5187aa171
Ubiratan Tavares <ust1973@gmail.com>
2025-04-21 21:55:15 -0300

    implementação do modulo detector-borda.v

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,19 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
     
-//insira seu código aqui
+    reg [1:0] entrada_anterior;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            detector = 2'b00;
+            entrada_anterior = 2'b00;
+        end else begin
+            detector = (~entrada_anterior) & entrada;
+            entrada_anterior = entrada;
+        end
+    end
 
 endmodule
